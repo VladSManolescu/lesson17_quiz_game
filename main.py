31e382c934b7f60554d48a196504fac7736041dc
Vlad Manolescu <vladstefan1221@yahoo.com>
2024-03-25 20:30:10 +0200

    added first feature of the game

--- main.py
+++ main.py
@@ -2,16 +2,15 @@
 # A user has to login , if player then he can play , if admin cand add questions
 import json
 import users
-
-
-
-
-
-
+import game
 
 
 if __name__ == '__main__':
     welcome_msg = "Welcome to Quiz Game"
     print(f"{len(welcome_msg) * '='}{welcome_msg}{len(welcome_msg) * '='}")
 
-    users.login()
+    current_player = users.login()
+
+    while True:
+        print(f"Let's play {list(current_player.keys())[0]}")
+        game.run_game(current_player)
