1a6787bc8e76537ca924ea1dac39f3d71d80b009
Vlad Manolescu <vladstefan1221@yahoo.com>
2024-03-27 18:33:55 +0200

    finished developing change_highscore function also adds date
    game now wait 2 seconds and allows player to exit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # create a quiz game with admin and players.
 # A user has to login , if player then he can play , if admin cand add questions
 import json
+import time
+
 import users
 import game
 
@@ -14,3 +16,8 @@ if __name__ == '__main__':
     while True:
         print(f"Let's play {list(current_player.keys())[0]}")
         game.run_game(current_player)
+        time.sleep(2)
+        user_pick = input("Do you want to play again? Y/N: ")
+        if user_pick.lower() == "n":
+            break
+
